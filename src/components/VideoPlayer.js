9de9254d9c25f13e9a884125d49239c15ba2ac06
Athul Mohan <athul@buymeacoffee.com>
2024-11-30 15:05:10 +0530

    new UI updates and video control button removed, integrated to react player changes to trigger controls

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -5,25 +5,47 @@ import { useUser } from '../context/UserContext';
 import './VideoPlayer.css';
 import { socket } from "../socket/socket";
 import { useVideoCall } from "../context/VideoCallContext";
+import { FaBars, FaLink, FaSignOutAlt } from 'react-icons/fa'; // Import icons
 
 const VideoPlayer = () => {
   const playerRef = useRef(null);
-  const [playing, setPlaying] = useState(false);
+  const drawerRef = useRef(null);
+  const [playing, setPlaying] = useState('pause');
   const [url, setURL] = useState("");
+  const [isTriggeringAllowed, setTriggeringAllowed] = useState(true);
+  const { userData, clearUserData } = useUser();
+  const { onCleanup, setIsVideoCallActive } = useVideoCall();
   const [playURL, setPlayURL] = useState(
     "https://www.youtube.com/watch?v=d9IKg-nizhQ"
   );
-  const { userData, clearUserData } = useUser();
-  const { onCleanup, setIsVideoCallActive } = useVideoCall();
+  const [isModalOpen, setIsModalOpen] = useState(false); // State for modal
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false); // State for drawer
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (drawerRef.current && !drawerRef.current.contains(event.target)) {
+        setIsDrawerOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [drawerRef]);
 
   useEffect(() => {
     socket.on("videoAction", (data) => {
       if (data.action === "play") {
-        setPlaying(true);
+        setPlaying('play');
       } else if (data.action === "pause") {
-        setPlaying(false);
+        setPlaying('pause');
       } else if (data.action === "seek") {
         playerRef.current.seekTo(data.time, "seconds");
+        setTriggeringAllowed(false)
+        setTimeout(() => {
+          setTriggeringAllowed(true)
+        }, 3000);
       }else if (data.action?.includes("setVideo")) {
         console.log(data?.action)
         const v=data?.action?.split('@')[1]
@@ -35,19 +57,21 @@ const VideoPlayer = () => {
     return () => socket.off("videoAction");
   }, []);
 
-  const handlePlayPause = () => {
-    const action = playing ? "pause" : "play";
-    setPlaying(!playing);
+  const handlePlayPause = (action,time) => {
+    if (!isTriggeringAllowed) return;
+    !!time&&socket.emit("videoTriggered", { action: "seek", time,roomId:userData?.channel });
     socket.emit("videoTriggered", { action,roomId:userData?.channel });
+    setPlaying(action);
   };
 
   const handleSeek = (time) => {
-    console.log(time);
     socket.emit("videoTriggered", { action: "seek", time,roomId:userData?.channel });
   };
 
   const newVideo = () => {
+    setPlayURL(url);
     socket.emit("videoTriggered", { action:'setVideo@'+url,roomId:userData?.channel });
+    setIsModalOpen(false)
   }
 
   const handleLogout = async() => {
@@ -62,68 +86,83 @@ const VideoPlayer = () => {
   return (
     <div className="video-page">
       <div className="video-header">
-        <div className="user-info">
-          <span className="channel-name">Channel: {userData.channel}</span>
-          <span className="username">User: {userData.username}</span>
+        <div className="brand">
+          <img src="/logo192.png" alt="logo" className="brand-logo" />
+          <span className="brand-name">WatchParty</span>
         </div>
-        <button className="logout-btn" onClick={handleLogout}>
-          Logout
-        </button>
+        <div className="header-icons">
+          <div className="icon" onClick={() => setIsModalOpen(!isModalOpen)}>
+            <FaLink />
+          </div>
+          <div className="menu-icon" onClick={(e) => {if(e.defaultPrevented) return;setIsDrawerOpen(!isDrawerOpen)}}>
+            <FaBars />
+          </div>
+        </div>
       </div>
-      
+
+      <div className={`drawer ${isDrawerOpen ? 'open' : ''}`} style={{ zIndex: 1000 }} ref={drawerRef}>
+        <div className="drawer-content">
+          <div className="drawer-header">
+            <img src="/logo192.png" alt="logo" className="brand-logo" />
+            <span className="drawer-title">WatchParty</span>
+          </div>
+          <div className="drawer-body">
+            <div className="user-info">
+              <p>Username: {userData?.username}</p>
+              <p>Channel: {userData?.channel}</p>
+            </div>
+            <button className="logout-btn" onClick={handleLogout}>
+              <FaSignOutAlt /> Logout
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className={`modal ${isModalOpen ? 'open' : ''}`} style={{ zIndex: 1000 }}>
+        <div className="modal-content">
+          <div className="modal-header">
+            <span className="modal-title">Set Video URL</span>
+          </div>
+          <div className="video-controls">
+            <div className="url-input-container">
+              <input
+                type="text"
+                value={url}
+                onChange={(e) => setURL(e.target.value)}
+                placeholder="Enter video URL..."
+                className="url-input"
+              />
+              <div className="video-buttons">
+                <button 
+                  className="set-video-btn"
+                  onClick={() => newVideo()}
+                >
+                  Done
+                </button>
+                <button className="close-btn" onClick={() => setIsModalOpen(false)}>Cancel</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <div className="video-container">
         <ReactPlayer
           ref={playerRef}
           url={playURL}
-          playing={playing}
+          stopOnUnmount={true}
+          onPlay={(e) => handlePlayPause("play",playerRef.current.getCurrentTime())}
+          onPause={() => handlePlayPause("pause")}
+          playing={playing==='play'}
           controls={true}
-          // onSeek={(e) => handleSeek(e)}
+          onSeek={(e) => console.log("Seeked: ", e)}
           progressInterval={1000}
           width="100%"
           height="100%"
+          allow="autoplay; encrypted-media"
+          allowFullScreen
           className="react-player"
         />
-      </div>
-
-      <div className="video-controls">
-        <div className="url-input-container">
-          <input
-            type="text"
-            value={url}
-            onChange={(e) => setURL(e.target.value)}
-            placeholder="Enter video URL..."
-            className="url-input"
-          />
-          <button 
-            className="set-video-btn"
-            onClick={() => newVideo()}
-          >
-            Set Video
-          </button>
-        </div>
-        <div className="seek-buttons-container">
-        <button 
-          className="seek-backward-btn"
-          style={{marginRight: '10px'}}
-          onClick={()=>handleSeek(playerRef.current.getCurrentTime()-10)}
-        >
-          {"<< 10s"}
-        </button>
-        <button 
-          className="play-pause-btn"
-          onClick={handlePlayPause}
-        >
-          {playing ? "Pause" : "Play"}
-        </button>
-
-        <button 
-          className="seek-forward-btn"
-          style={{marginLeft: '10px'}}
-          onClick={()=>handleSeek(playerRef.current.getCurrentTime()+10)}
-        >
-          {"10s >>"}
-        </button>
-        </div>
       </div>
     </div>
   );
